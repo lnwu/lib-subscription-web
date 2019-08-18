16a9feeb95aa08bb1f10bb0535a774ccb04e986e
lnwu <wind2729@gmail.com>
2019-08-18 14:26:51 +0800

    Li: setup header

--- pages/srp/index.tsx
+++ pages/srp/index.tsx
@@ -1,10 +1,12 @@
 import React from "react"
 import Head from "../../shared/components/Head"
+import Header from "../../shared/components/header"
 
 const SRP: React.FC = () => {
   return (
     <>
       <Head title="SRP" />
+      <Header />
       <div>SRP</div>
     </>
   )
