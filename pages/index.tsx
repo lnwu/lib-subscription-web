ed343234aeb4425bf971bdc12cce0326f935725b
lnwu <wind2729@gmail.com>
2019-08-18 13:46:36 +0800

    Li: add head component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
-import Head from '../components/head'
-import Nav from '../components/nav'
+import Head from './shared/components/Head';
 
 const Home = () => {
   const [date, setDate] = useState<any>(null);
@@ -18,7 +17,6 @@ const Home = () => {
   return (
     <div>
       <Head title="Home" />
-      <Nav />
 
       <div className="hero">
         <h1 className="title">Welcome to Next!</h1>
---
+++ pages/shared/components/Head.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import NextHead from 'next/head'
+
+interface IProps {
+  title?: string
+}
+
+const Head: React.FC<IProps> = ({ title = "Lib Subscription" }) => (
+  <NextHead>
+    <meta charSet="UTF-8" />
+    <title>{title}</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="icon" href="/static/favicon.ico" />
+  </NextHead>
+)
+
+export default React.memo(Head)
